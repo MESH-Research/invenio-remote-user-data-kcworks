eac192a6b101ba3af8b2eb3b22e62d9e5f037d0f
Ian Scott <scottianw@gmail.com>
2024-01-26 18:09:40 -0500

    More setup

---
+++ setup.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2024 MESH Research
+#
+# invenio-remote-user-data is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Extension to draw user data into InvenioRDM from a remote source"""
+
+from setuptools import find_packages, setup
+
+readme = open("README.md").read()
+history = open("CHANGES.md").read()
+
+install_requires = [
+    "click>=7.0",
+]
+
+tests_require = ["pytest>=7.3.2", "pytest-runner"]
+
+dev_requires = []
+
+extras_require = {"tests": tests_require, "dev": dev_requires}
+
+extras_require["all"] = []
+for reqs in extras_require.values():
+    extras_require["all"].extend(reqs)
+
+packages = find_packages(include=["invenio_groups", "invenio_groups.*"])
+
+setup(
+    name="invenio-remote-user-data",
+    description=__doc__,
+    long_description=readme + "\n\n" + history,
+    long_description_content_type="text/markdown",
+    keywords="invenio inveniordm users",
+    license="MIT",
+    author="Mesh Research",
+    author_email="scottia4@msu.edu",
+    url="https://github.com/MESH-Research/invenio-remote-user-data",
+    packages=packages,
+    include_package_data=True,
+    platforms="any",
+    install_requires=install_requires,
+    extras_require=extras_require,
+    tests_require=tests_require,
+    setup_requires=["pytest-runner"],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)

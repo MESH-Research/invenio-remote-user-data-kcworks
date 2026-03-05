e93ba9dec768d6efe8a686169fd6257b6a2ee2a7
Ian W. Scott <scottianw@gmail.com>
2026-03-05 17:01:20 -0500

    fix(roles): Regularizing admin role name to "administration" as in core.

--- tests/conftest.py
+++ tests/conftest.py
@@ -637,14 +637,14 @@ def minimal_record():
 
 @pytest.fixture()
 def admin_role_need(db):
-    """Store 1 role with 'administration-access' ActionNeed.
+    """Store 1 role with 'administration' that has administration-access ActionNeed.
 
     WHY: This is needed because expansion of ActionNeed is
          done on the basis of a User/Role being associated with that Need.
          If no User/Role is associated with that Need (in the DB), the
          permission is expanded to an empty list.
     """
-    role = Role(name="administration-access")
+    role = Role(name="administration")
     db.session.add(role)
 
     action_role = ActionRoles.create(
@@ -674,7 +674,7 @@ def admin(UserFixture, app, db, admin_role_need):
 
     datastore = app.extensions["security"].datastore
     _, role = datastore._prepare_role_modify_args(
-        u.user, "administration-access"
+        u.user, "administration"
     )
 
     UserIdentity.create(u.user, "knowledgeCommons", "myuser")
